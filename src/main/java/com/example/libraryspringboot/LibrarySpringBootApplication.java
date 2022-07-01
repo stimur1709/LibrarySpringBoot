eaa6617e0e7489cc37d3f53fdd36f3e00cb42a91
stimur1709 <stimur1709@mail.ru>
2022-07-01 16:17:58 +0700

    Initial commit

---
+++ src/main/java/com/example/libraryspringboot/LibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryspringboot/LibrarySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
