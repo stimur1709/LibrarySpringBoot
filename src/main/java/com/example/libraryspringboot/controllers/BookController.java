11d02fb24fad295ece0f8b2ffe02d41dfc4a8e14
stimur1709 <stimur1709@mail.ru>
2022-07-01 19:49:46 +0700

    Добавлены страницы книг

--- src/main/java/com/example/libraryspringboot/controllers/BookController.java
+++ src/main/java/com/example/libraryspringboot/controllers/BookController.java
@@ -1,10 +1,17 @@
 package com.example.libraryspringboot.controllers;
 
+import com.example.libraryspringboot.models.Book;
 import com.example.libraryspringboot.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
@@ -12,5 +19,51 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @GetMapping
+    public String getBooksPage(Model model) {
+        model.addAttribute("books", bookService.getBooks());
+        return "books/index";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "books/new";
+    }
+
+    @PostMapping("/new")
+    public String createBook(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "books/new";
+        bookService.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("{id}")
+    public String showBook(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookService.findById(id));
+        return "books/show";
+    }
+
+    @GetMapping("{id}/edit")
+    public String editBook(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookService.findById(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("{id}/edit")
+    public String changeBook(@PathVariable("id") int id, @ModelAttribute("book") @Valid Book book,
+                             BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return "books/edit";
+        bookService.change(id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("{id}/delete")
+    public String deleteBook(@PathVariable("id") int id) {
+        bookService.delete(id);
+        return "redirect:/books";
     }
 }
--- src/main/java/com/example/libraryspringboot/service/BookService.java
+++ src/main/java/com/example/libraryspringboot/service/BookService.java
@@ -1,8 +1,11 @@
 package com.example.libraryspringboot.service;
 
+import com.example.libraryspringboot.models.Book;
 import com.example.libraryspringboot.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
@@ -12,5 +15,26 @@ public class BookService {
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    public Book findById(int id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void change(int id, Book book) {
+        book.setId(id);
+        bookRepository.save(book);
+    }
+
+    public void delete(int id) {
+        bookRepository.deleteById(id);
     }
 }
